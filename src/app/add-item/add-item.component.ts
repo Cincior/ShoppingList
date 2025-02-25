9503cfe0f5db44264e297686b529f9801f3449b6
Cincior <krzycho75000@gmail.com>
2025-02-25 22:32:28 +0100

    added list component and some validation

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -1,21 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms'
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-add-item',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './add-item.component.html',
   styleUrl: './add-item.component.css'
 })
 export class AddItemComponent {
+  submitted: boolean;
 
   addItemForm = new FormGroup({
-    itemName: new FormControl(''),
-    quantity: new FormControl(0),
+    itemName: new FormControl('', Validators.required),
+    itemQuantity: new FormControl(0, [Validators.required, Validators.min(1)]),
   });
+  constructor() {
+    this.submitted = false;
+  }
 
   addItem() {
-    console.log("added!");
+    this.submitted = true;
+    if(this.addItemForm.get('itemName')?.errors || this.addItemForm.get('itemQuantity')?.errors) {
+      return;
+    }
+    console.log(this.addItemForm.value.itemName + " " + this.addItemForm.value.itemQuantity);
+  
+    this.addItemForm.reset({
+      itemName: '',
+      itemQuantity: 0,
+    });
+    this.submitted = false
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { AddItemComponent } from "./add-item/add-item.component";
+import { ItemListComponent } from "./item-list/item-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, AddItemComponent],
+  imports: [RouterOutlet, HeaderComponent, AddItemComponent, ItemListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
